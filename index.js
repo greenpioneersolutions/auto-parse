5609db89620c7764b89e21553dc614818ac695dc
Jason Humphrey <humphrey.jason32@gmail.com>
2018-03-14 00:42:48 -0500

    #11 updated the comments

--- index.js
+++ index.js
@@ -2,14 +2,36 @@ module.exports = parse
 
 var typpy = require('typpy')
 
+/**
+ * 
+ * @name stripTrimLower
+ * @function
+ * @param {Value} value strip trim & lower case the string
+ * @return {Value} parsed string
+ *
+ */
 function stripTrimLower (value) {
   return value.replace(/[""'']/ig, '').trim().toLowerCase()
 }
-
+/**
+ * 
+ * @name toBoolean
+ * @function
+ * @param {Value} value parse to boolean
+ * @return {Boolean} parsed boolean
+ *
+ */
 function toBoolean (value) {
   return checkBoolean(value) || false
 }
-
+/**
+ * 
+ * @name checkBoolean
+ * @function
+ * @param {Value} value is any value
+ * @return {Boolean} is a boolean value 
+ *
+ */
 function checkBoolean (value) {
   if (!value) {
     return false
@@ -24,7 +46,14 @@ function checkBoolean (value) {
 
   return null
 }
-
+/**
+ * 
+ * @name parseObject
+ * @function
+ * @param {Value} value parse object
+ * @return {Value} parsed object
+ *
+ */
 function parseObject (value) {
   if (typpy(value, Array)) {
     return value.map(function (n, key) {
@@ -36,22 +65,40 @@ function parseObject (value) {
     }
     return value
   }
-
   return {}
 }
-
+/**
+ * 
+ * @name parseFunction
+ * @function
+ * @param {Value} value function
+ * @return {Value} returned value from the called value function
+ *
+ */
 function parseFunction (value) {
   return parse(value())
 }
-
+/**
+ * 
+ * @name parseType
+ * @function
+ * @param {Value} value inputed value
+ * @param {Type} type  inputed type
+ * @return {Value} parsed type
+ *
+ */
 function parseType (value, type) {
-  // Currently they send a string - handle String or Number or Boolean?
+  /**
+   *  Currently they send a string - handle String or Number or Boolean?
+   */
   if ((value && value.constructor === type) || typpy(value, type)) {
     return value
   }
 
   var typeName = type
-  // Convert the constructor into a string
+  /**
+   * Convert the constructor into a string
+   */
   if (type && type.name) {
     typeName = type.name.toLowerCase()
   }
@@ -66,46 +113,35 @@ function parseType (value, type) {
       if (typpy(value, Function)) {
         return value
       }
-
       return function (cb) {
         if (typeof cb === 'function') {
           cb(value)
         }
         return value
       }
-
     case 'date':
       return new Date(value)
-
     case 'object':
       var jsonParsed
       try {
         jsonParsed = JSON.parse(value)
       } catch (e) {}
-
       if (typpy(jsonParsed, Object) || typpy(jsonParsed, Array)) {
         return parse(jsonParsed)
       } else if (!typpy(jsonParsed, 'undefined')) {
         return {}
       }
-
       return parseObject(value)
-
     case 'boolean':
       return toBoolean(value)
-
     case 'number':
       return Number(value)
-
     case 'undefined':
       return undefined
-
     case 'null':
       return null
-
     case 'array':
       return [value]
-
     default:
       if (typeof type === 'function') {
         return new type(value) // eslint-disable-line
@@ -113,42 +149,61 @@ function parseType (value, type) {
       throw new Error('Unsupported type.')
   }
 }
-
+/**
+ * Parse
+ * auto-parse any value you happen to send in 
+ * (String, Number, Boolean, Array, Object, Function, undefined and null). 
+ * You send it we will try to find a way to parse it. 
+ * We now support sending in a string of what type
+ * (e.g. "boolean") or constructor (e.g. Boolean)
+ *
+ * Usage:
+ *
+ * ```js
+ * Parse({}) // => "object"
+ * Parse('42'); // => 42
+ * Parse.get('[]'); // => []
+ * ```
+ *
+ * @name Parse
+ * @function
+ * @param {Value} input The input value.
+ * @param {Constructor|String} target The target type.
+ * @return {String|Function|Date|Object|Boolean|Number|Undefined|Null|Array} 
+ */
 function parse (value, type) {
   if (type) {
     return parseType(value, type)
   }
-
   var orignalValue = value
-
-  // PRE RULE - check for null be cause null can be typeof object which can  through off parsing
+  /**
+   *  PRE RULE - check for null be cause null can be typeof object which can  through off parsing
+   */
   if (value === null) {
     return null
   }
-
-  // TYPEOF SECTION - Use to check and do specific things based off of know the type
-  // Check against undefined
+  /**
+   * TYPEOF SECTION - Use to check and do specific things based off of know the type
+   * Check against undefined
+   */
   if (value === void 0) {
     return undefined
   }
-
   if (typeof value === 'number' || typeof value === 'boolean') {
     return value
   }
-
   if (typeof value === 'function') {
     return parseFunction(value)
   }
-
   if (typeof value === 'object') {
     return parseObject(value)
   }
-
-  // STRING SECTION - If we made it this far that means it is a string that we must do something with to parse
+  /**
+   * STRING SECTION - If we made it this far that means it is a string that we must do something with to parse
+   */
   if (value === 'NaN') {
     return NaN
   }
-
   var jsonParsed = null
   try {
     jsonParsed = JSON.parse(value)
@@ -157,29 +212,27 @@ function parse (value, type) {
   if (jsonParsed && typeof jsonParsed === 'object') {
     return parse(jsonParsed)
   }
-
   value = stripTrimLower(value)
-
   if (value === 'undefined' || value === '') {
     return undefined
   }
-
   if (value === 'null') {
     return null
   }
-
-  // Order Matter because if it is a one or zero boolean will come back with a awnser too. if you want it to be a boolean you must specify
+  /**
+   * Order Matter because if it is a one or zero boolean will come back with a awnser too. if you want it to be a boolean you must specify
+   */
   var num = Number(value)
   if (typpy(num, Number)) {
     return num
   }
-
   var boo = checkBoolean(value)
   if (typpy(boo, Boolean)) {
     return boo
   }
-
-  // DEFUALT SECTION - bascially if we catch nothing we assume that you just have a string
+  /**
+   * DEFAULT SECTION - bascially if we catch nothing we assume that you just have a string
+   */
   var string = String(orignalValue)
   return string
 }
